70427676a9a9b3253ddac2be7d43afc475f86a33
ahfaezal <ahfaezal@gmail.com>
2026-01-18 18:50:14 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ClusterPage from "./ClusterPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ClusterPage />
   </React.StrictMode>
 );
