ddcf328d2f33147ea6a2b47f0166a6fd0489bfe4
ahfaezal <ahfaezal@gmail.com>
2026-01-18 23:47:22 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import LiveBoard from "./LiveBoard.jsx";     // pastikan fail & path betul
-import ClusterPage from "./ClusterPage.jsx"; // pastikan fail & path betul
-
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Default: terus ke Live Board */}
-        <Route path="/" element={<Navigate to="/board" replace />} />
-
-        {/* Paparan 1: Live Board */}
-        <Route path="/board" element={<LiveBoard />} />
-
-        {/* Paparan 3: Clustering CU */}
-        <Route path="/cluster" element={<ClusterPage />} />
-
-        {/* fallback */}
-        <Route path="*" element={<Navigate to="/board" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
